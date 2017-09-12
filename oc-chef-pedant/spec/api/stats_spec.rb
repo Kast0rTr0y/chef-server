ea70212f44dcee7860ba6c44aaa38a2cf9a23943
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2017-09-12 18:02:12 -0400

    Make dynamic rspec tests
    Signed-off-by: Kartik Null Cating-Subramanian <ksubramanian@chef.io>

--- oc-chef-pedant/spec/api/stats_spec.rb
+++ oc-chef-pedant/spec/api/stats_spec.rb
@@ -2,230 +2,71 @@ require 'pedant/rspec/common'
 
 describe "/_stats API endpoint", :stats do
 
+  response_type_map = {
+    "erlang_vm_time_correction" => "UNTYPED",
+    "erlang_vm_thread_pool_size" => "GAUGE",
+    "erlang_vm_threads" => "UNTYPED",
+    "erlang_vm_smp_support" => "UNTYPED",
+    "erlang_vm_schedulers_online" => "GAUGE",
+    "erlang_vm_schedulers" => "GAUGE",
+    "erlang_vm_process_limit" => "GAUGE",
+    "erlang_vm_process_count" => "GAUGE",
+    "erlang_vm_port_limit" => "GAUGE",
+    "erlang_vm_port_count" => "GAUGE",
+    "erlang_vm_logical_processors_online" => "GAUGE",
+    "erlang_vm_logical_processors_available" => "GAUGE",
+    "erlang_vm_logical_processors" => "GAUGE",
+    "erlang_vm_ets_limit" => "GAUGE",
+    "erlang_vm_statistics_wallclock_time_milliseconds" => "COUNTER",
+    "erlang_vm_statistics_runtime_milliseconds" => "COUNTER",
+    "erlang_vm_statistics_run_queues_length_total" => "GAUGE",
+    "erlang_vm_statistics_reductions_total" => "COUNTER",
+    "erlang_vm_statistics_bytes_output_total" => "COUNTER",
+    "erlang_vm_statistics_bytes_received_total" => "COUNTER",
+    "erlang_vm_statistics_garbage_collection_bytes_reclaimed" => "COUNTER",
+    "erlang_vm_statistics_garbage_collection_words_reclaimed" => "COUNTER",
+    "erlang_vm_statistics_garbage_collection_number_of_gcs" => "COUNTER",
+    "erlang_vm_statistics_context_switches" => "COUNTER",
+    "erlang_vm_memory_system_bytes_total" => "GAUGE",
+    "erlang_vm_memory_processes_bytes_total" => "GAUGE",
+    "erlang_vm_ets_tables" => "GAUGE",
+    "erlang_vm_dets_tables" => "GAUGE",
+    "erlang_vm_memory_bytes_total" => "GAUGE",
+    "erlang_vm_memory_atom_bytes_total" => "GAUGE",
+    "erchef_pooler_queued_requestors_max" => "GAUGE",
+    "erchef_pooler_queued_requestors" => "GAUGE",
+    "erchef_pooler_members_max" => "GAUGE",
+    "erchef_pooler_members_free" => "GAUGE",
+    "erchef_pooler_members_in_use" => "GAUGE",
+    "pg_stat_n_conns" => "GAUGE",
+    "pg_stat_n_active_conns" => "GAUGE",
+    "pg_stat_tidx_blks_hit" => "COUNTER",
+    "pg_stat_tidx_blks_read" => "COUNTER",
+    "pg_stat_toast_blks_hit" => "COUNTER",
+    "pg_stat_toast_blks_read" => "COUNTER",
+    "pg_stat_idx_blks_hit" => "COUNTER",
+    "pg_stat_idx_blks_read" => "COUNTER",
+    "pg_stat_heap_blocks_hit" => "COUNTER",
+    "pg_stat_heap_blocks_read" => "COUNTER",
+    "pg_stat_n_dead_tup" => "GAUGE",
+    "pg_stat_n_live_tup" => "GAUGE",
+    "pg_stat_n_tup_del" => "COUNTER",
+    "pg_stat_n_tup_upd" => "COUNTER",
+    "pg_stat_n_tup_ins" => "COUNTER",
+    "pg_stat_tup_fetch" => "COUNTER",
+    "pg_stat_idx_scan" => "COUNTER",
+    "pg_stat_seq_tup_read" => "COUNTER",
+    "pg_stat_seq_scan" => "COUNTER"
+  }
+
   let(:request_url) { "#{platform.server}/_stats" }
   let(:response_body) do
-    [
+    response_type_map.map do |name, type|
       {
-        "name" => "erlang_vm_time_correction",
-        "type" => "UNTYPED",
-      },
-      {
-        "name" => "erlang_vm_thread_pool_size",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erlang_vm_threads",
-        "type" => "UNTYPED",
-      },
-      {
-        "name" => "erlang_vm_smp_support",
-        "type" => "UNTYPED",
-      },
-      {
-        "name" => "erlang_vm_schedulers_online",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erlang_vm_schedulers",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erlang_vm_process_limit",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erlang_vm_process_count",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erlang_vm_port_limit",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erlang_vm_port_count",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erlang_vm_logical_processors_online",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erlang_vm_logical_processors_available",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erlang_vm_logical_processors",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erlang_vm_ets_limit",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erlang_vm_statistics_wallclock_time_milliseconds",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "erlang_vm_statistics_runtime_milliseconds",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "erlang_vm_statistics_run_queues_length_total",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erlang_vm_statistics_reductions_total",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "erlang_vm_statistics_bytes_output_total",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "erlang_vm_statistics_bytes_received_total",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "erlang_vm_statistics_garbage_collection_bytes_reclaimed",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "erlang_vm_statistics_garbage_collection_words_reclaimed",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "erlang_vm_statistics_garbage_collection_number_of_gcs",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "erlang_vm_statistics_context_switches",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "erlang_vm_memory_system_bytes_total",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erlang_vm_memory_processes_bytes_total",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erlang_vm_ets_tables",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erlang_vm_dets_tables",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erlang_vm_memory_bytes_total",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erlang_vm_memory_atom_bytes_total",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erchef_pooler_queued_requestors_max",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erchef_pooler_queued_requestors",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erchef_pooler_members_max",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erchef_pooler_members_free",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "erchef_pooler_members_in_use",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "pg_stat_n_conns",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "pg_stat_n_active_conns",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "pg_stat_tidx_blks_hit",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "pg_stat_tidx_blks_read",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "pg_stat_toast_blks_hit",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "pg_stat_toast_blks_read",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "pg_stat_idx_blks_hit",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "pg_stat_idx_blks_read",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "pg_stat_heap_blocks_hit",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "pg_stat_heap_blocks_read",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "pg_stat_n_dead_tup",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "pg_stat_n_live_tup",
-        "type" => "GAUGE",
-      },
-      {
-        "name" => "pg_stat_n_tup_del",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "pg_stat_n_tup_upd",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "pg_stat_n_tup_ins",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "pg_stat_tup_fetch",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "pg_stat_idx_scan",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "pg_stat_seq_tup_read",
-        "type" => "COUNTER",
-      },
-      {
-        "name" => "pg_stat_seq_scan",
-        "type" => "COUNTER",
+        "name" => name,
+        "type" => type
       }
-    ]
-  end
-
-  let(:response_type_map) do
-    Hash[response_body.map { |stat| [stat["name"], stat["type"]] }]
+    end
   end
 
   let(:boolean_stats) { ["erlang_vm_time_correction", "erlang_vm_threads", "erlang_vm_smp_support"] }
@@ -238,16 +79,20 @@ describe "/_stats API endpoint", :stats do
     })
   end
 
-  it "returns metrics of the correct type", :smoke do
-    response = JSON.parse(get(request_url, platform.non_admin_user))
-    response_type_map.each do |name, type|
+  response_type_map.each do |name, type|
+    it "returns metrics for #{name} typed as #{type}", :smoke do
+      response = JSON.parse(get(request_url, platform.non_admin_user))
       stat = response.find { |s| s["name"] == name }
       expect(stat["metrics"]).not_to be_empty
       stat["metrics"].each do |metric|
         expect(metric).to have_key("value")
-        if type == "GAUGE" || type == "COUNTER"
+        case type
+        when "GAUGE"
+          expect(Float(metric["value"])).to be_a(Numeric)
+        when "COUNTER"
           expect(Integer(metric["value"])).to be_a(Integer)
-        elsif type == "UNTYPED" && boolean_stats.include?(name)
+        when "UNTYPED"
+          expect(boolean_stats).to include(name)
           expect(metric["value"]).to eq("1").or eq("0")
         else
           raise "Unimplemented test for metric type #{type}"
